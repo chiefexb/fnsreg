4110d4f6ea1cf17d79d0f6eb4a8a5637bde6c62f
chiefexb <chief_exb@mail.ru>
2014-12-30 21:06:32 +0300

    Продолжение работы

--- process.py
+++ process.py
@@ -16,11 +16,12 @@ class Profiler(object):
         print "Elapsed time:",time.time() - self._startTime # {:.3f} sec".forma$
         st="Elapsed time:"+str(time.time() - self._startTime) # {:.3f} sec".for$
         logging.info(st)
+const={}
 def main():
 #Обработка параметров
  #print len (sys.argv)
  if len(sys.argv)<=1:
-  print ("getfromint: нехватает параметров\nИспользование: getfromint ФАЙЛ_КОНФ$
+  print ("getfromint: нехватает параметров")
   sys.exit(2)
  print sys.argv[1]
 #Открытие файла конфигурации
@@ -41,21 +42,21 @@ def main():
  concodepage=dbparams.find('connection_codepage').text
  codepage=dbparams.find('codepage').text
  database=dbparams.find('database').text
-logpar=cfgroot.find('logging')
- log_path=logpar.find('log_path').text
- log_file=logpar.find('log_file').text
- log_file2=logpar.find('log_file2').text
+ #log_file=logpar.find('log_file').text
+ #log_file2=logpar.find('log_file2').text
 #Определяем тип и путь файла
  filepar=cfgroot.find('file')
- filecodepage=filepar.find('codepage').text
  output_path=filepar.find('output_path').text
  input_path=filepar.find('input_path').text
  input_arc_path=filepar.find('input_path_arc').text
- input_err_path=filepar.find('input_path_err').text
- filetype=filepar.find('type').text
- filenum=filepar.find('numeric').text
+ input_spo_path=filepar.find('input_path_spo').text
+ input_spo__arc_path=filepar.find('input_path_spo_arc').text
  #Определение схемы файла должна быть ветка для типов файлов пока разбираем xml
  filescheme=filepar.findall('scheme')
+ zaproses=filescheme[0][0]
+ print zaproses.tag
+ zapros=zaproses.getchildren()[0]
+ print zapros.tag
  return
 if __name__ == "__main__":
     main()
