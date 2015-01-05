0f5d929755bc6d2ce3b0cf5309f101d6d2c0dd7d
chiefexb <chief_exb@mail.ru>
2015-01-05 16:54:28 +0300

    Изменения в БД и начал делать загрузку запросов. Добавил генераторы, анализ конфигурации запроса, делаю генерацию запроса на вставку запросов.

--- process.py
+++ process.py
@@ -8,6 +8,7 @@ import fdb
 import sys
 from os import *
 import logging
+fields=['packet_id','packet_date','request_id','request_date','debitor_name','debitor_birthday','debitor_inn']
 class Profiler(object):
     def __enter__(self):
         self._startTime = time.time()
@@ -16,7 +17,19 @@ class Profiler(object):
         print "Elapsed time:",time.time() - self._startTime # {:.3f} sec".forma$
         st="Elapsed time:"+str(time.time() - self._startTime) # {:.3f} sec".for$
         logging.info(st)
-const={}
+def getgenerator(cur,gen):
+ sq="SELECT GEN_ID("+gen+", 1) FROM RDB$DATABASE"
+ try:
+  cur.execute(sq)
+ except:
+  print "err"
+ cur.execute(sq)
+ r=cur.fetchall()
+ try:
+  g=r[0][0]
+ except:
+  g=-1
+ return g
 def main():
 #Обработка параметров
  #print len (sys.argv)
@@ -54,9 +67,27 @@ def main():
  #Определение схемы файла должна быть ветка для типов файлов пока разбираем xml
  filescheme=filepar.findall('scheme')
  zaproses=filescheme[0][0]
- print zaproses.tag
- zapros=zaproses.getchildren()[0]
- print zapros.tag
+ if sys.argv[1]=='upload':
+  print zaproses.tag
+  zapros=zaproses.getchildren()[0]
+  zaprostag=zapros.tag
+  fld={}
+  for ch in zapros.getchildren():
+   if ch.text in fields:
+    fld[ch.text]=ch.tag
+  print fld
+  ff=listdir(input_spo_path)
+  print ff[0]
+  fff=ff[0]
+  xmlfile=file(input_spo_path+fff) #'rr4.xml')
+  xml=etree.parse(xmlfile)
+  xmlroot=xml.getroot()
+  print xmlroot.tag
+  zaporoses=xmlroot.findall(zaprostag)
+  print len (zaporoses)
+  sqlreq=INSERT INTO REQUESTS (ID, DATE_LOAD, PACKET_ID, PACKET_DATE, REQUEST_ID, REQUEST_DATE, DEBITOR_NAME, DEBITOR_BIRTHDAY, DEBITOR_INN, PROCESSED, DATE_PROCESSED) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+
+ #EndIF
  return
 if __name__ == "__main__":
     main()
